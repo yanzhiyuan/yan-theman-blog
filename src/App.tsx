6459e4f8e290cb0c98f250a46f1c7b21324e4da8
Yan, Zhi Yuan <zhiyyan@gmail.com>
2018-08-16 22:33:40 +0800

    add tech skill chips

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import AppBackground from './component/AppBackground';
 import 'typeface-cabin-sketch';
 import Typography from '@material-ui/core/Typography';
 import DropdownTab from './component/DropdownTab';
-import {Button, Paper, Collapse } from '../node_modules/@material-ui/core';
+import {Button, Paper, Collapse, createMuiTheme, MuiThemeProvider } from '../node_modules/@material-ui/core';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
 import * as model from './model/PortfolioModel';
 import ChipList from './component/ChipList';
+import grey from '@material-ui/core/colors/grey';
+import blueGrey from '@material-ui/core/colors/blueGrey';
 
-// const theme = createMuiTheme({
-//   palette: {
-//     type: 'dark',
-//   }
-// });
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: grey,
+    secondary: blueGrey,
+  }
+});
 
 interface IAppState {
   selectedTab: string;
@@ -31,12 +35,30 @@ class App extends React.Component<any,IAppState> {
 
     this.techSkill = new Map<string, model.TechSkill[]>();
     this.techSkill.set('Database', [
-      {name:'MongoDB', iconName:'mongoPng'}
+      {name:'MongoDB', iconName:'mongoPng'},
+      {name:'Sql Server', iconName:'sqlServer'},
+      {name:'Oracle', iconName:'oracle'}
+    ]);
+    this.techSkill.set('Langurage', [
+      {name:'C#', iconName:'cSharp'},
+      {name:'TypeScript', iconName:'typescript'},
+      {name:'C++', iconName:'cpp'},
+      {name:'Objective-C', iconName:'objectiveC'},
+      {name:'Swift', iconName:'swift'},
+      {name:'Go lang', iconName:'goLang'},
+      {name:'R', iconName:'R'}
+    ]);
+    this.techSkill.set('Framework', [
+      {name:'Dot Net Core', iconName:'dotNetCore'},
+      {name:'.Net Framework', iconName:'dotNet'},
+      {name:'React', iconName:'react'},
+      {name:'iOS / Cocoa Touch', iconName:'ios'},
     ]);
   }
 
   public render() {
     return (
+      <MuiThemeProvider theme={theme}>
       <div style={{
         height:'100vh', 
         width:'100vw', 
@@ -49,22 +71,22 @@ class App extends React.Component<any,IAppState> {
        <div style={{textAlign:'center'}}>
        <div style={{display:'flex', justifyContent:'flex-end'}}>
          <Button>
-          <Typography style={{fontFamily:'Cabin Sketch', color:'white'}} variant="button">
+          <Typography style={{fontFamily:'Cabin Sketch'}} variant="button">
             Contact
           </Typography>
-           <MailOutlineIcon style={{color:'white'}}/>
+           <MailOutlineIcon />
          </Button>
          <Button>
-          <Typography style={{fontFamily:'Cabin Sketch', color:'white'}} variant="button">
+          <Typography style={{fontFamily:'Cabin Sketch'}} variant="button">
             CV
           </Typography>
-           <FormatListBulletedIcon style={{color:'white'}}/>
+           <FormatListBulletedIcon/>
          </Button>
        </div>   
-        <Typography style={{fontFamily:'Cabin Sketch', color:'white'}} variant="display4">
+        <Typography style={{fontFamily:'Cabin Sketch'}} variant="display4">
           Yan, Zhi Yuan
         </Typography>
-        <Typography style={{color:'white', textAlign:'center'}} variant="body1">
+        <Typography style={{textAlign:'center'}} variant="body1">
           Hi, my name is Yan. blah blah blah blah
         </Typography>
       </div>
@@ -88,6 +110,7 @@ class App extends React.Component<any,IAppState> {
         </Paper>
       </Collapse>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/component/ChipList.tsx
+++ src/component/ChipList.tsx
@@ -2,6 +2,19 @@ import * as React from 'react';
 import * as model from '../model/PortfolioModel';
 import { List, Typography, ListItem, Chip, Avatar, Divider } from '@material-ui/core';
 import mongoPng from '../asset/mongoDB.jpg';
+import cSharp from '../asset/cSharp.png';
+import dotNet from '../asset/dotNet.png';
+import dotNetCore from '../asset/dotnetCore.png';
+import goLang from '../asset/goLang.png';
+import objectiveC from '../asset/objective-c.png';
+import oracle from '../asset/oracle.png';
+import R from '../asset/R.png';
+import react from '../asset/react.png';
+import sqlServer from '../asset/sql-server.png';
+import swift from '../asset/swift.png';
+import typescript from '../asset/typescript.png';
+import cpp from '../asset/cpp.png';
+import ios from '../asset/ios.png';
 
 interface IChipListProps{
     chipList: Map<string, model.TechSkill[]>;
@@ -9,6 +22,19 @@ interface IChipListProps{
 
 const imgs = {
     mongoPng,
+    cSharp,
+    dotNet, 
+    dotNetCore, 
+    goLang,
+    objectiveC,
+    oracle,
+    R,
+    react,
+    sqlServer,
+    swift,
+    typescript,
+    cpp,
+    ios,
 }
 
 export default class ChipList extends React.Component<IChipListProps>{
@@ -30,7 +56,7 @@ export default class ChipList extends React.Component<IChipListProps>{
                 (
                     <ListItem key={'list_'+key} style={{display:'flex', flexDirection:'column', alignItems:'flex-start'}}>
                     <div>
-                    <Typography variant="title" style={{color:'white'}}>
+                    <Typography variant="title" >
                     {key}
                     </Typography>
                     </div>
@@ -40,10 +66,11 @@ export default class ChipList extends React.Component<IChipListProps>{
                             avatar={<Avatar src={imgs[tech.iconName]} /> }
                             label={tech.name}
                             key={'tech_chip' + tech.name}
+                            style={{margin:'5px'}}
                         />
                     )}
                     </div>
-                    <Divider light={true}/>
+                    <Divider/>
                     </ListItem>
                 )
             );
